5cebb9a440c78b16d58afb55d48334b088b8fd70
gigigirl <10566007@ntub.edu.tw>
2016-11-22 17:34:48 +0800

    Update idscsv.js

--- idscsv.js
+++ idscsv.js
@@ -1,11 +1,9 @@
+
  function IDS(){
-  d3.csv("https://gigigirl.github.io/Software-Engineering/dataI.csv",function(polygonDataI)
-  // function polyonDataD1(){d3.csv("https://gigigirl.github.io/Software-Engineering/dataD1.csv",function(polygonDataD1)}
-  // function polyonDataD2(){d3.csv("https://gigigirl.github.io/Software-Engineering/datad2.csv",function(polygonDataD2)}
-  // function polyonDataS(){d3.csv("https://gigigirl.github.io/Software-Engineering/dataS.csv",function(polygonDataS))
- 
-  // function pologonWord(I,color)
-  // {
+  d3.csv("https://gigigirl.github.io/Software-Engineering/dataI.csv", 
+
+  function pologonWord(I,color)
+  {
    //var svgrect = d3.select("body").append("svg")
        //.attr("height",320)
        //.attr("width",130);
@@ -21,9 +19,10 @@
   var svgrect = d3.select("body").append("svg")
        .attr("height",227);
        .attr("width",460);
-  // pologonWord(polygonDataI,"brown");
+  pologonWord(polygonDataI,"brown");
   // pologonWord(polygonDataD1,"black");
   // pologonWord(polygonDataD2,"purple");
   // pologonWord(polygonDataS,"purple");
-}
+});
+
 
