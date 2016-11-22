6871e907bf775693d33b83af7046e78421dc84e0
gigigirl <10566007@ntub.edu.tw>
2016-11-22 18:46:33 +0800

    Update idscsv.js

--- idscsv.js
+++ idscsv.js
@@ -1,9 +1,14 @@
 
- function IDS(color){
-  d3.csv("https://gigigirl.github.io/Software-Engineering/dataI.csv", 
+ function IDS(){
+  var polygonDataI="https://gigigirl.github.io/Software-Engineering/dataI.csv"
 
-  // function pologonWord(color)
-  // {
+  var polygonDataD1="https://gigigirl.github.io/Software-Engineering/dataD1.csv"
+  var polygonDataD2="https://gigigirl.github.io/Software-Engineering/dataD2.csv"
+
+  var polygonDataS="https://gigigirl.github.io/Software-Engineering/dataS.csv"
+
+  function pologonWord(I,color)
+  {
    //var svgrect = d3.select("body").append("svg")
        //.attr("height",320)
        //.attr("width",130);
@@ -15,14 +20,25 @@
         .attr("stroke","none")
         .attr("fill",color)
         .attr("stroke-width",1)
-  var svgrect = d3.select("body").append("svg")
+  }
+
+  d3.csv(pologonWord(polygonDataI,"brown"),function(data){
+      var svgrect = d3.select("body").append("svg")
        .attr("height",227);
        .attr("width",460);
-  // pologonWord("brown");
-  // pologonWord(polygonDataD1,"black");
-  // pologonWord(polygonDataD2,"purple");
-  // pologonWord(polygonDataS,"purple");
- );
-}
-
-
+  });
+  d3.csv(pologonWord(polygonDataD1,"black"),function(data){
+      var svgrect = d3.select("body").append("svg")
+       .attr("height",227);
+       .attr("width",460);
+  });
+  d3.csv(pologonWord(polygonDataD2,"purple"),function(data){
+      var svgrect = d3.select("body").append("svg")
+       .attr("height",227);
+       .attr("width",460);
+  });
+  d3.csv(pologonWord(polygonDataS,"purple"),function(data){
+      var svgrect = d3.select("body").append("svg")
+       .attr("height",227);
+       .attr("width",460);
+   });
